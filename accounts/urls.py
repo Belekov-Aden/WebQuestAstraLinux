9542c24f129a4e980bd240bacba13910de21e4aa
B.Aden <belekovaden31@gmail.com>
2025-02-17 09:45:51 +1100

    add image and start do presentation pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.presentation, name='home'),
     path('signup/', views.register, name='signup'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
