b3d71d69dec47dde3bae27b266b8b38ed00b4382
B.Aden <belekovaden31@gmail.com>
2025-02-21 00:29:09 +1100

    [1 STEP] - Сохранение прогресса по этапу 1, добавление в бД, в случае повтороного прохождение, перезапись

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class QuizResult(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    step = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(7)
+        ]
+    )
+    score = models.IntegerField()
+    total_questions = models.IntegerField()
+    date_taken = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Этап {self.step}: {self.user.username} - {self.score}/{self.total_questions}"
+
+    class Meta:
+        verbose_name = 'Результаты по квестам'
