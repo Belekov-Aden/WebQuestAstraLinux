8f3ffbb366536576185da1baac906d27dadb9383
B.Aden <belekovaden31@gmail.com>
2025-02-23 18:20:07 +1100

    add modules in view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.core.serializers import json
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import RegisterUserForm, LoginForm
+from .models import QuizResult
 
 
 def register(request):
